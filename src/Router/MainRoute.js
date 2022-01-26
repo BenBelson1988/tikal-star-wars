6b94abcbb5fca869da4c4dcf15e2048e63b81c43
Ben <belson1988@gmail.com>
2022-01-26 23:03:48 +0200

    Puplic updated

--- src/Router/MainRoute.js
+++ src/Router/MainRoute.js
@@ -18,6 +18,9 @@ export default () => {
       <Route exact path="/">
         <Redirect to={"task1"} />
       </Route>
+      {/* <Route exact path="/tikal-star-wars">
+        <Redirect to={"task1"} />
+      </Route> */}
       <Route
         exact
         path="/task1"
